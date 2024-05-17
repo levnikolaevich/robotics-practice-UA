8a3c53baa328c070072147acb7f4d3da673c3057
levnikolaevich <levnikolaevich@ya.ru>
2024-05-17 09:15:20 +0200

    Capture video and show camera robot

--- scripts/capture_video.py
+++ scripts/capture_video.py
@@ -1,31 +1,48 @@
 #!/usr/bin/env python3
 
 import rospy
-import cv2
+import cv2, os
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 
 def video_publisher():
     # Inicializa un nodo ROS
     rospy.init_node('video_publisher', anonymous=True)
+    
 
-    # TODO Crea un publicador en el tópico /operator/image
+    # Crea un publicador en el tópico /operator/image
+    image_pub = rospy.Publisher('/operator/image', Image, queue_size=10)
 
-    # TODO Configura la captura de video desde la webcam (o desde un vídeo)
-    cap = None
+    # Configura la captura de video desde la webcam (o desde un vídeo)
+    # cap = cv2.VideoCapture(0)
+    video_path = '/home/docker/catkin_ws/src/robotica_inteligente/scripts/gestos.mp4'
+    print(os.path.exists(video_path)) 
+    
+    cap = cv2.VideoCapture(video_path) 
+    if not cap.isOpened():
+        rospy.logerr("Unable to open video source")
+        exit()
+
 
     # Crea una instancia de CvBridge para convertir las imágenes de OpenCV a mensajes de ROS
     bridge = CvBridge()
 
-    # Define la tasa de publicación (e.g., 10 Hz)
+    # Define la tasa de publicación (e.g., 30 Hz)
     rate = rospy.Rate(30)
 
     while not rospy.is_shutdown():
-        # TODO Captura un frame de la webcam
-                
-        # TODO Convierte el frame de OpenCV a un mensaje ROS
+        # Captura un frame de la webcam
+        print("Opened:", cap.isOpened()) 
+        ret, frame = cap.read()
+        if not ret:
+            rospy.logerr("Failed to capture image")
+            break
 
-        # TODO Publica el mensaje en el tópico
+        # Convierte el frame de OpenCV a un mensaje ROS
+        image_msg = bridge.cv2_to_imgmsg(frame, "bgr8")
+
+        # Publica el mensaje en el tópico
+        image_pub.publish(image_msg)
 
         # Espera para cumplir con la tasa de publicación
         rate.sleep()
--- scripts/show_camera_robot.py
+++ scripts/show_camera_robot.py
@@ -3,7 +3,7 @@
 # Importar librerías necesarias
 import rospy
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 import cv2
 
 # Función callback para cuando se recibe una imagen
@@ -14,19 +14,21 @@ def image_callback(msg):
     except CvBridgeError as e:
         print(e)
     else:
-        # TODO Mostrar la imagen en una ventana de OpenCV y esperar 1 ms para que OpenCV procese los eventos de la GUI
-        return
+        # Mostrar la imagen en una ventana de OpenCV y esperar 1 ms para que OpenCV procese los eventos de la GUI
+        cv2.imshow("Camera View", cv_image)
+        cv2.waitKey(1)  # Espere 1 ms entre cada marco para procesar eventos de GUI
 
 def main():
     rospy.init_node('image_viewer', anonymous=True)
-    # TODO Suscribir al tópico de ROS que tiene las imágenes
+    # Suscribir al tópico de ROS que tiene las imágenes
+    rospy.Subscriber('/camera/color/image_raw', Image, image_callback)
     
     # Prevenir que Python cierre hasta que el nodo es detenido
     try:
         rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows()  # Asegurarse de cerrar todas las ventanas de OpenCV
 
 if __name__ == '__main__':
     main()
